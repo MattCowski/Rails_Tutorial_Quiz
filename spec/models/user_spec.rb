d3eadecb62e6d6c27e0a47212f63ef8960a3bb14
Tony Tau <tonytaudesign@gmail.com>
2013-09-05 12:08:35 -0500

    think it is actually working now with add_secure_password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,20 @@
 require 'spec_helper'
 
 describe User do
-	before {@user = User.new(name: "Bob", email: "bob@email.com")}
+	before {@user = User.new(name: "Bob", email: "bob@email.com", password: "foobar",
+                          password_confirmation: "foobar")}
 	# create user
 	subject {@user}
 
 	it {should respond_to(:name)}
 	it {should respond_to(:email)}
+  it {should respond_to(:password_digest)}
+  it {should respond_to(:password)}
+  it {should respond_to(:password_confirmation)}
 	
+  it { should be_valid }
+
+
 	describe "when user not present" do
 		before {@user.name = ""}
 		it {should_not be_valid}
@@ -50,9 +57,25 @@ describe User do
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
-
-    it { should_not be_valid }
   end
+
+  describe "when password is not present" do
+    before {@user.password = "", @user.password_confirmation=""}
+    it {should_not be_valid}
+  end
+
+  describe "when password doesn't match confirmation" do 
+    before {@user.password_confirmation= "mismatch"}
+    it {should_not be_valid}
+  end
+
+
+
+
+
+
+
+
 
 	
 end
