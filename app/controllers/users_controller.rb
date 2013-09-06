318261270249f85bbe27ae3cfd62bf686de6c098
Tony Tau <tonytaudesign@gmail.com>
2013-09-06 16:08:25 -0500

    asasa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,25 @@ class UsersController < ApplicationController
   end
 
   def create
+	  @user = User.new(user_params)
+	  if @user.save
+	  	flash[:success] = "Welcome to the Sample App!"
+	  	redirect_to @user
+	  else
+      #flash[:error] = ""
+	  	render 'new'
+	  end
+
   end
+
+  def edit
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 
 end
